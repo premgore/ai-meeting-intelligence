69ec54eed3b61b136aea46a208302c9f7cfeca91
PREM <prem@PREMs-MacBook-Air.local>
2026-07-28 21:13:58 +0530

    Set up PostgreSQL connection with SQLAlchemy

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -4,12 +4,14 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     APP_NAME: str
     APP_ENV: str
-    APP_VERSION: str  
+    APP_VERSION: str
     APP_PORT: int
+    DATABASE_URL: str
 
     model_config = SettingsConfigDict(
         env_file=".env",
-        env_file_encoding="utf-8"
+        env_file_encoding="utf-8",
+        extra="ignore",
     )
 
 
